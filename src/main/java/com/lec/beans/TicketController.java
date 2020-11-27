cb6ca139c2939116dbe3d4e6d6ae326919b2a9be
admin <admin@DESKTOP-R2NKGM0>
2020-11-27 09:47:46 +0900

    예매, 좌석, 결제 형태구성

---
+++ src/main/java/com/lec/beans/TicketController.java
@@ -0,0 +1,28 @@
+package com.lec.beans;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ticket")
+public class TicketController {
+	
+	@RequestMapping("/ticketing")
+	public void ticketing() {
+		
+		
+	}
+	
+	@RequestMapping("/choiceSeat")
+	public void choiceSeat() {
+		
+		
+	}
+	
+	@RequestMapping("/payment")
+	public void pay() {
+		
+		
+	}
+	
+}
